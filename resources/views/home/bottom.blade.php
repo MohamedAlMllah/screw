042ab501afb7471f563cdfc30cd4eddcc19b6854
Mohamed Ali <mohamedalmllah@gmail.com>
2023-10-21 16:34:54 +0300

    ui fix

--- resources/views/home/bottom.blade.php
+++ resources/views/home/bottom.blade.php
@@ -8,12 +8,11 @@
             @endif
             <div class="text-center">
                 {{$hand->index}}
-                @if($game->getPlayerInOrder($numberOfPlayers/2,$user)->hands->count() == $hand->index)
+                @if($user->hands->count() == $hand->index)
                 <img src="{{asset('images/cards/back.png')}}" class="card-img-top img-fluid rounded-0" alt="...">
                 @else
                 <img src="{{asset('images/cards/backHalf.png')}}" class="card-img-top img-fluid rounded-0" alt="...">
                 @endif
-                <!--<img src="{{asset($hand->card->image)}}" class="card-img-top img-fluid" alt="...">-->
                 @if($user->participant->is_turn && $skill == 'normal')
                 <a href="{{ route('bsra', [$hand->id]) }}" class="btn btn-outline-warning mt-1">بصرة</a>
                 @elseif($user->participant->is_turn && ($skill == 'bossFeWrqtak' || ($skill == 'kaabDayer' && in_array($user->id, json_decode($user->participant->kaab_dayer, true)))))
